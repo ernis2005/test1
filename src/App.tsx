b7e43d67900469c2e011504e1a5d5a4de6c21e06
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-10-07 16:29:49 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.scss';
+import {AiOutlineArrowRight} from 'react-icons/ai'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+      <div className='block'>
+        <span className='BlockBg'>
+          <p>ORACLEDIGITAL</p>
+          <p>ORACLEDIGITAL</p>
+        </span>
+        <div className='blockInfo'>
+          <h2>
+            /получи <br />
+            работу в it<br />
+            программистом вы it <br />
+            helllo компании
+            {/* <button>Связаться с нами  <AiOutlineArrowRight/></button>  */}
+          </h2>
+          <p>Quis fringilla convallis et vitae volutpat at porttitor. Est tincidunt massa aliquam sed enim rhoncus. <br /> Id nullam sollicitudin aliquet in. </p>
+        </div>
+      </div>
+      <div>
+        hello
+      </div>
+      <div>
+        hello
+      </div>
+      <div>
+        hello
+      </div>
+      <div>
+        hello
+      </div>
+      <div>
+        hello
+      </div>
+      <div>
+        hello
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Page.scss'
+import { RiMenu3Line } from "react-icons/ri";
+export const Navigation = () => {
+  return (
+    <header className='Navigation'>
+      <nav>
+        <h1>
+          opacle <span>edigital</span>
+        </h1>
+        <ul>
+          <li>Главная</li>
+          <li>О компании</li>
+          <li>Услуги</li>
+          <li>Контакты</li>
+        </ul>
+
+        <p>Get started</p>
+        <div className='MenuIcon'>
+          <RiMenu3Line />
+        </div>
+      </nav>
+
+    </header>
+  )
+}
