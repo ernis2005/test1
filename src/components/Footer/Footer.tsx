9475eb7587f7c59fbedb55f36d392e9d217a8fd7
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-10-12 17:41:07 +0600

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,7 +24,20 @@ const Footer = () => {
                         </button>
                     </div>
                  </div>
-                  <div>2</div>
+                  <div className='FooterBlock2'>
+                        <div>
+                           1
+                        </div>
+                        <div>
+                            2
+                        </div>
+                        <div>
+                            3
+                        </div>
+                        <div>
+                            4
+                        </div>
+                  </div>
         </div>
     </div>
   )
