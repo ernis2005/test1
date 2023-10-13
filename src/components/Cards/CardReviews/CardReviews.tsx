f8b78f1350f341bc5f074daa4a0aab3e5798dc9b
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-10-13 17:40:37 +0600

    fix

--- src/components/Cards/CardReviews/CardReviews.tsx
+++ src/components/Cards/CardReviews/CardReviews.tsx
@@ -10,22 +10,31 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { Pagination } from 'swiper/modules';
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 
 let Svg =()=> {
     return<svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
     <path d="M22.5 10.25H14.4375L12 2.75L9.5625 10.25H1.5L8.0625 14.75L5.53125 22.25L12 17.5625L18.4688 22.25L15.9375 14.75L22.5 10.25Z" fill="#ECCD2B" stroke="#ECCD2B" stroke-width="3" stroke-linejoin="round"/>
   </svg>
 }
-export const CardReviews = () => {
+export const CardReviews = () => { 
+    const [swiper, setSwiper] = React.useState<any>();
 
     let name = 'CardReviews'
     return (
         <div className={`${name}`}>
+            <div className='buttons'>
+                <button onClick={() => swiper?.slidePrev()}><BiChevronLeft/></button>
+                <button onClick={() => swiper?.slideNext()}><BiChevronRight/></button>
+            </div>
              <Swiper
         slidesPerView={2}
         centeredSlides={true}
         spaceBetween={30}
         grabCursor={true}
+        onSwiper={(s:any) => {
+            setSwiper(s);
+          }}
         breakpoints={{
             320:{
                 slidesPerView: 1,
