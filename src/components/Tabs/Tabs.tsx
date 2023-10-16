6708cf9d0918866bd9a7aeca796cce9f460b7f71
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-10-16 12:41:46 +0600

    fix

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -5,6 +5,7 @@ export const Tabs = () => {
   let [state, setState] = useState(0)
   return (
     <div className='Tabs'>
+      
       <h2 className='TabsH2'>Часто задаваемые вопросы</h2>
       <p>Maecenas ut magna netus purus enim. Et sit est arcu facilisi tortor ipsum sem. Neque porta morbi ullamcorper blandit libero quam ligula ut aenean. </p>
       <div>
